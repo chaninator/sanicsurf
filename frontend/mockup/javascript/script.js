81318bc7e5d252864889b89cf7d05e64168aa098
Andrew Chan <accsuperstar@gmail.com>
2017-05-01 18:12:06 -0500

    making responsiveness a thing - CCS

--- frontend/mockup/javascript/script.js
+++ frontend/mockup/javascript/script.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
-    $("#sanicTheme").get(0).play();
-    $('.sanic').click(function(e) {
-      e.preventDefault();
-      
-    });
-  )
 
-
-
+  // $("#sanicTheme").get(0).play();
+  $('.sanic-hidden').hide();
+  $('.active').show();
+  // hide show sanics based on selected
+  $('.sanic').click(function(e) {
+    e.preventDefault();
+    console.log('clicked');
+    var sanicId = $(this).data('sanic');
+    console.log(sanicId);
+    $('.sanic-hidden').hide();
+    $('.sanic-display .info'+sanicId).show();
+  });
 
 });
