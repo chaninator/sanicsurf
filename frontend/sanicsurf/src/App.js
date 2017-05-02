3dce3a44613b04c10ae73766e08f7e0323e1f893
Robert Saunders <rtsaunders19@gmail.com>
2017-05-02 08:45:36 -0500

    set up login and wait page

--- frontend/sanicsurf/src/App.js
+++ frontend/sanicsurf/src/App.js
@@ -1,19 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import { firebase, auth } from './utils/firebase';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  componentWillMount() {
+    auth.onAuthStateChanged(currentUser => {
+      if (currentUser) {
+        console.log('Logged in:', currentUser);
+        this.setState({ currentUser });
+      } else {
+        this.setState({ currentUser: null });
+      }
+    });
+  }
+
+  loginButtonClicked(e) {
+    e.preventDefault();
+
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  logoutButtonClicked(e) {
+    e.preventDefault();
+
+    auth.signOut();
+  }
+
+  sessionButton() {
+    if (!this.state.currentUser ) {
+      return <LoginButton loginButtonClicked={ this.loginButtonClicked }>Log in with Google</LoginButton>;
+    } else {
+      return (
+        <div className="profile">
+
+          <div className="info col-md-5">
+            <p className="displayName"> { this.state.currentUser.displayName } </p>
+            <p className="waiting">  Waiting on game to begin... </p>
+            <LogoutButton logoutButtonClicked={ this.logoutButtonClicked }>Log out</LogoutButton>
+          </div>
+          <div className="loader col-md-2"><img src="./images/loading.gif"/></div>
+          <div className="space col-md-5"></div>
+
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <section>
+        <div className="App">
+            { this.sessionButton() }
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </section>
     );
   }
 }
