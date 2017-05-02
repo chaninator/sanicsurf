27326d0242df78bb66046cf1ed5a62fcf0fb2539
Ronak Singh <rssobti@outlook.com>
2017-05-02 09:22:40 -0500

    added components

---
+++ frontend/sanicsurf/src/components/WaitingPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class WaitingPage extends Component {
+  render(){
+    return(
+      <div>
+        <h2>Wait for race to start</h2>
+      </div>
+    )
+  }
+}
+
+export default WaitingPage;
--- frontend/sanicsurf/src/index.js
+++ frontend/sanicsurf/src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
+import SanicProfileList from './components/SanicProfileList';
+
+
 
 ReactDOM.render(
-  <App />,
+  (<Router history={hashHistory}>
+    <Route path="/" component={App}>
+    </Route>
+    <Route path="./components/SanicRacerList" component={SanicRacerList} />
+  </Router>)
   document.getElementById('root')
 );
