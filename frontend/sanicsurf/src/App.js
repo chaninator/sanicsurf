8718e2a6a89df3597e3acd5d0367bb11d90c4071
Ronak Singh <rssobti@outlook.com>
2017-05-02 10:28:02 -0500

    added routes

--- frontend/sanicsurf/src/App.js
+++ frontend/sanicsurf/src/App.js
@@ -2,31 +2,11 @@ import React, { Component } from 'react';
 import { Link, IndexLink } from 'react-router';
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login';
-import SanicProfileList from './components/SanicProfileList';
 
 
 
 
 class App extends Component {
-
-  addNum(){
-    this.setState={
-
-    }
-  }
-
-  constructor(props){
-  super(props);
-  this.state = {
-    query: '',
-    artist: 'WEEKND',
-    vote: 0
-  }
-
-
-
-}
 
   render() {
     return (
@@ -34,26 +14,14 @@ class App extends Component {
 
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Sanic-Surf</h2>
         </div>
 
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <Login/>
-
-        <SanicProfileList addNum={this.addNum()}/>
-
-
-
         <ul className="header">
-      
-        <li><Link to="/SanicProfileList" activeClassName="active" className="navlink">SanicProfileList</Link></li>
+      <li><Link to="/SanicRacerList" activeClasseName="active">SanicRacerList</Link></li>
+      <li><Link to="/SanicProfileList" activeClasseName="active">SanicProfileList</Link></li>
       </ul>
-      <div className="content">
-        {this.props.children}
-      </div>
+
 
       </div>
     );
---
+++ frontend/sanicsurf/src/components/Admin.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Admin extends Component {
+
+
+  render(){
+    return(
+      <div>
+          <h2>Sanic #1</h2>
+          <p className='description'>U DA ADMIN</p>
+
+      </div>
+    )
+  }
+}
+
+export default Admin;
