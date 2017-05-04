ccaba828911ca16c37ffa56dcb1f4b9332530943
Ronak Singh <rssobti@outlook.com>
2017-05-04 09:22:18 -0500

    added admin.js

--- frontend/sanicsurf/src/components/Admin.js
+++ frontend/sanicsurf/src/components/Admin.js
@@ -1,17 +1,20 @@
 import React, {Component} from 'react';
+import {Link, Redirect } from 'react-router';
+
 
 class Admin extends Component {
 
   startGame(){
-    console.log('Starting Game')
+    console.log('Starting Game');
+
   }
 
   render(){
     return(
       <div>
           <h2>AdMAN</h2>
-          <p className='description'>U DA ADMIN</p>
-          <button onClick={()=> this.startGame()}>Start</button>
+          <p className='description'>U DA AD MAN</p>
+          <button onClick={()=> this.startGame()}><li><Link to="/SanicAdminSelect" activeClassName="active" className="navlink">Start</Link></li></button>
 
       </div>
     )
