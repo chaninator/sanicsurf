1bc38860445de918cdd2f704c19cbf356357a0dd
Andrew Chan <accsuperstar@gmail.com>
2017-05-04 22:53:45 -0500

    fixed mobile user view

--- frontend/sanicsurf/src/components/WaitingPage.js
+++ frontend/sanicsurf/src/components/WaitingPage.js
@@ -10,22 +10,26 @@ class WaitingPage extends Component {
 
     const isMobile = window.innerWidth <= 500;
     if (isMobile) {
+    return (
       <div className="background">
         <div className="profile">
           <div className="info col-md-5">
             <p className="displayName">
-              {this.props.displayName}
+              Welcome {this.props.displayName}!
             </p>
             <p className="waiting">
               Waiting on game to begin...
             </p>
+            <div className="logoutBtn">
             <LogoutButton logoutButtonClicked={this.props.logoutButtonClicked}>Log out</LogoutButton>
+          </div>
           </div>
           <div className="loader col-md-2"><img src="./images/loading.gif" alt=""/></div>
           <div className="space col-md-5"></div>
           {/*<Link to="/SanicProfileList" activeClasseName="active">SanicProfileList</Link>*/}
         </div>
       </div>
+    )
     } else {
       return (
         <div className="background">
